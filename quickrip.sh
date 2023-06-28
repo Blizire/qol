9bf12b30d8f359c1d4c5e7a1ec8e61448e0d41ed
Trenton Stiles <stiles.trenton@gmail.com>
2023-06-27 19:20:58 -0500

    added basrc and *nix setup scrip

---
+++ quickrip.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Run these commands first just to make sure everything is updated
+#
+# sudo apt-get update -y
+# sudo apt-get upgrade -y
+
+# software installation order does matter in some cases
+software=(
+	"gcc"
+	"build-essential"
+	"git"
+	"python3-pip"
+	"checksec"
+	"python3-ropgadget"
+	"python3-dev"
+	"libssl-dev"
+	"libfi-dev"
+	"ffmpeg"
+	"wireshark"
+	"cowsay"
+	"net-tools"
+	"screenfetch"
+)
+
+for i in "${software[@]}"; do
+  sudo apt-get install "$i" -y
+done
+
+mkdir ~/software
+
+sudo snap install --classic code
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pwntools
+
+cd ~/software
+git clone https://github.com/radareorg/radare2
+cd ~/software/radare2/sys
+./install.sh
+
+cd ~/software
+git clone https://github.com/pwndbg/pwndbg
+cd ~/software/pwndbg/
+./setup.sh
+
+# end of software installation
+
+# bashrc additions
+~/.bashrc >> "cowsay welcome back Trenton"
+~/.bashrc >> "alias py='python3'"
+ssh-keygen
